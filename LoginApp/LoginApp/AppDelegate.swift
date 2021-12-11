c7e594209084b042819ec2f0169485cfacd9ccbd
Nikita12G <87696400+Nikita12G@users.noreply.github.com>
2021-12-11 11:02:13 +0700

    Login App

---
+++ LoginApp/LoginApp/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  LoginViewController.swift
+//
+//  Created by Никита Гуляев on 26.08.2021.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
